51dce14971b398f876881273ca4983ed4743dcd4
PeterR <piotrrutkowski97@gmail.com>
2020-04-29 01:29:23 +0200

    up

--- mpld3_data1.py
+++ mpld3_data1.py
@@ -1,30 +1,13 @@
-from utils import import_data, save_plot
+from utils import import_data, build_plot_dataset_1,save_plot
 from mpld3 import plugins
-import mpld3
-import numpy as np
 import matplotlib.pyplot as plt
 
 data, labels = import_data(1)
-years = np.arange(1960,2019,1)
 
 fig, ax = plt.subplots()
+plot = build_plot_dataset_1(data, fig, ax)
 
-ax.set_ylim(0, 1500000000)
-ax.set_xlim(1960,2019)
-ax.set_ylabel('Population [billion]')
-ax.set_xticks(np.arange(1960,2019,5))
-ax.set_yticks(np.arange(0,1500000000,200000000))
-y_labels = ['0','0.2','0.4','0.6','0.8','1.0','1.2','1.4']
-x_labels = [str(year) for year in np.arange(1960,2019,5)]
-ax.set_yticklabels(y_labels)
-ax.set_xticklabels(x_labels)
-ax.set_title('The evolution of populations of most populous countries in 1960')
-
-fig.subplots_adjust(right=0.7)
-line_plots = ax.plot(years, data.T, lw=2, alpha=0.8)
-interactive_legend = plugins.InteractiveLegendPlugin(line_plots, labels)
+interactive_legend = plugins.InteractiveLegendPlugin(plot, labels)
 plugins.connect(fig, interactive_legend)
-
-plt.tight_layout(2)
 
 save_plot('mpld3_data1', fig)
--- utils.py
+++ utils.py
@@ -10,17 +10,35 @@ def import_data(dataset_label):
     data = np.asarray(data.drop(['country'], axis=1))
     return data, labels
 
+def build_plot_dataset_1(data, fig, ax):
+    ax.set_ylim(0, 1500000000)
+    ax.set_xlim(1960, 2020)
+    ax.set_ylabel('Population [billion]')
+    ax.set_xticks(np.arange(1960, 2021, 10))
+    ax.set_yticks(np.arange(0, 1500000000, 200000000))
+    y_labels = ['0', '0.2', '0.4', '0.6', '0.8', '1.0', '1.2', '1.4']
+    x_labels = [str(year) for year in np.arange(1960, 2021, 10)]
+    ax.set_yticklabels(y_labels)
+    ax.set_xticklabels(x_labels)
+    ax.set_title('The evolution of populations of most populous countries in 1960')
+
+    years = np.arange(1960, 2019, 1)
+    fig.subplots_adjust(right=0.7)
+    line_plots = ax.plot(years, data.T, lw=2, alpha=0.8)
+
+    return line_plots
+
 def save_plot(filename, fig):
     if len(argv) > 1:
         if argv[1] == '0':
             show(fig)
         else:
-            plt.savefig('plots/' + filename + '.png', dpi=200)
+            #plt.savefig('plots/' + filename + '.png', dpi=200)
             save_html(fig, 'plots/' + filename + '.html')
-            print('plots/' + filename + '.png')
+            #print('plots/' + filename + '.png')
             print('plots/' + filename + '.html')
     else:
-        plt.savefig('plots/' + filename + '.png', dpi=200)
+        #plt.savefig('plots/' + filename + '.png', dpi=200)
         save_html(fig, 'plots/' + filename + 'html')
-        print('plots/' + filename + '.png')
+        #print('plots/' + filename + '.png')
         print('plots/' + filename + '.html')
