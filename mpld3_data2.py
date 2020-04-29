20bd183917c2e867634ac2d0ff255b9db7ecef78
PeterR <piotrrutkowski97@gmail.com>
2020-04-29 14:01:15 +0200

    mpld3

---
+++ mpld3_data2.py
@@ -0,0 +1,13 @@
+from utils import import_data, build_plot_dataset_1, save_plot_mpld3
+from mpld3 import plugins
+import matplotlib.pyplot as plt
+
+data, labels = import_data(2)
+
+fig, ax = plt.subplots()
+plot = build_plot_dataset_1(data, fig, ax)
+
+interactive_legend = plugins.InteractiveLegendPlugin(plot, labels)
+plugins.connect(fig, interactive_legend)
+
+save_plot_mpld3('mpld3_data2', fig)
--- utils.py
+++ utils.py
@@ -28,6 +28,25 @@ def build_plot_dataset_1(data, fig, ax):
 
     return line_plots
 
+def build_plot_dataset_1(data, fig, ax):
+    ax.set_ylim(0, 14000000)
+    ax.set_xlim(1960, 2020)
+    ax.set_ylabel('Population [million]')
+    ax.set_xticks(np.arange(1960, 2021, 10))
+    ax.set_yticks(np.arange(0, 14000000, 1500000))
+    y_labels = ['0','1.5','3.0','4.5','6.0','7.5','9.0','10.5','12.0','13.5']
+    x_labels = [str(year) for year in np.arange(1960, 2021, 10)]
+    ax.set_yticklabels(y_labels)
+    ax.set_xticklabels(x_labels)
+    ax.set_title('The evolution of populations of countries that were most \n' +
+              'similar population-wise to Hong Kong in 1984')
+
+    years = np.arange(1960, 2019, 1)
+    fig.subplots_adjust(right=0.7)
+    line_plots = ax.plot(years, data.T, lw=2, alpha=0.8)
+
+    return line_plots
+
 def save_plot_mpld3(filename, fig):
     if len(argv) > 1:
         if argv[1] == '0':
@@ -36,5 +55,5 @@ def save_plot_mpld3(filename, fig):
             save_html(fig, 'plots/' + filename + '.html')
             print('plots/' + filename + '.html')
     else:
-        save_html(fig, 'plots/' + filename + 'html')
+        save_html(fig, 'plots/' + filename + '.html')
         print('plots/' + filename + '.html')
